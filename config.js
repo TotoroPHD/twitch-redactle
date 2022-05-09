66ca41c55a71ca0597fa5165374145a209e7d530
TotoroPHD <64203596+TotoroPHD@users.noreply.github.com>
2022-05-09 23:39:47 +0200

    Adding CSS

--- config.js
+++ config.js
@@ -4,9 +4,57 @@ var channel = "TotoroPHD"
 // La première ligne est le titre à trouver
 
 var texte = `
-Ceci est un gros titre
-Coucou
-ça va bien ?
-Super.
-Comment est votre blanquette ?
+San Francisco
+C'est une maison bleue
+Adossée à la colline
+On y vient à pied
+On ne frappe pas
+Ceux qui vivent là
+Ont jeté la clé
+On se retrouve ensemble
+Après des années de route
+Et on vient s'asseoir
+Autour du repas
+Tout le monde est là
+À cinq heures du soir
+Quand San Francisco s'embrume
+Quand San Francisco s'allume
+San Franciso
+Où êtes-vous?
+Lizzard et Luc
+Psylvia, attendez-moi
+Nageant dans le brouillard
+Enlacés, roulant dans l'herbe
+On écoutera Tom à la guitare
+Phil à la quena jusqu'à la nuit noire
+Un autre arrivera
+Pour nous dire des nouvelles
+D'un qui reviendra
+Dans un an ou deux
+Puisqu'il est heureux
+On s'endormira
+Quand San Francisco se lève
+Quand San Francisco se lève
+San Francisco
+Où êtes-vous?
+Lizzard et Luc
+Psylvia, attendez-moi
+C'est une maison bleue
+Accrochée à ma mémoire
+On y vient à pied
+On ne frappe pas
+Ceux qui vivent là
+Ont jeté la clé
+Peuplée de cheveux longs
+De grands lits et de musique
+Peuplée de lumière
+Et peuplée de fous
+Elle sera dernière
+À rester debout
+Si San Francisco s'effondre
+Si San Francisco s'effondre
+San Francisco
+Où êtes-vous?
+Lizzard et Luc
+Psylvia, attendez-moi
 `
