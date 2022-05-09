d86186256f0d8dab49f35749b356fb4792c7ee92
TotoroPHD <64203596+TotoroPHD@users.noreply.github.com>
2022-05-09 18:48:22 +0200

    First commit

---
+++ config.js
@@ -0,0 +1,12 @@
+var channel = "TotoroPHD"
+
+//METTEZ LE TEXTE A TROUVER ENTRE LES DEUX `
+// La première ligne est le titre à trouver
+
+var texte = `
+Ceci est un gros titre
+Coucou
+ça va bien ?
+Super.
+Comment est votre blanquette ?
+`
